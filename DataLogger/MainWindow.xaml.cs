5e822659d78e918147b715c969c60d3653b77573
xkimi <kangmin@graftel.com>
2014-10-21 09:38:33 -0500

    initial Push master

--- DataLogger/MainWindow.xaml.cs
+++ DataLogger/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathNet.Numerics.LinearRegression;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
@@ -16,6 +17,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Research.DynamicDataDisplay;
+using Microsoft.Research.DynamicDataDisplay.DataSources;
 
 namespace DataLogger
 {
@@ -67,16 +70,17 @@ namespace DataLogger
 
     public partial class MainWindow : Window
     {
+        ObservableDataSource<Point> source1 = null;
 
         class TimeData
         {
             public string TimeStamp { get; set; }
-            public float  TestData { get; set; }
-            public float Diff { get; set; }
-            public float Elapse { get; set; }
+            public double  TestData { get; set; }
+            public double Diff { get; set; }
+            public double Elapse { get; set; }
 
 
-            public TimeData(DateTime currentTime, float data1, float elapse, float diff)
+            public TimeData(DateTime currentTime, double data1, double elapse, double diff)
             {
            
                 TimeStamp = currentTime.ToString("HH:mm:ss.fff");
@@ -94,8 +98,8 @@ namespace DataLogger
         public TimeLoop myDelegate;
         System.Timers.Timer timer = null;
         DateTime PrevTime;
-        float prevelapse = 0;
-        float currentelapse = 0;
+        double prevelapse = 0;
+        double currentelapse = 0;
         DateTime currentT;
         DateTime StartT;
         Stopwatch stopwatch;
@@ -145,6 +149,10 @@ namespace DataLogger
             _microStopwatch = new MicroLibrary.MicroStopwatch();
 
             _microTimer = new MicroLibrary.MicroTimer();
+
+            source1 = new ObservableDataSource<Point>();
+            source1.SetXYMapping(p => p);
+            plotter.AddLineGraph(source1);
         }
 
         private void Recieve(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
@@ -224,11 +232,11 @@ namespace DataLogger
 
         private void updateData(long elaspedTime, string data)
         {
-            float data1 = float.Parse(data);
+            double data1 = double.Parse(data);
 
             prevelapse = currentelapse;
 
-            currentelapse = (float)elaspedTime / 1000;
+            currentelapse = (double)elaspedTime / 1000;
            
             
 
@@ -391,14 +399,100 @@ namespace DataLogger
 
         private void GetInstFlowRate()
         {
+            //double[] ytestdata = new double[] { 2.0430489, 2.0430446, 2.0430402, 2.0430358, 2.0430245 };
+            //double[] xtestdata = new double[] { 0.19, 0.29, 0.39, 0.49, 0.59 };
+
+
+            double Delta_m;
+            double Delta_t;
+            int startpos = 1;
+            int endpos = 0;
             int count = expData.Count;
+            endpos = count;
+            double n = (double) (count - 1);
+            double Umrdg = 0, Um = 0, Usigma = 0, Udeltam = 0, OneOverDeltaT = 0, UdeltaT = 0.002, DTDM = 0, Uimpurity = 0.004, Uhumidity = 0.054, Umassrep = 0, Utimerep = 0.008, Usysrep = 0.128;
+            double U_eccentricity = 0.0007;
+            double U_linearity = 0.0005;
+            double U_sensitivity = 0.0008;
+            double U_caluncer = 0.0006;
+            double U_final = 0;
 
-            float result = 0;
+            if (count > 3)
+            {
 
-            result = (expData.ElementAt(count - 1).TestData - expData.ElementAt(count - 2).TestData) / (expData.ElementAt(count - 1).Elapse - expData.ElementAt(count - 2).Elapse);
+                double result = 0;
 
-            lblFlowRate.Content = result;
-            
+                double S_x =0, S_y = 0, S_xx = 0, S_xy = 0, S_yy = 0, beta = 0, alpha = 0, Se_beta2 = 0, Se_sigma2 = 0, Se_sigma = 0;
+
+                for (int i = startpos; i < endpos; i++)
+                {
+                    S_x += expData.ElementAt(i).Elapse / 1000;
+                    S_xx += Math.Pow(expData.ElementAt(i).Elapse / 1000, 2);
+                    S_y += expData.ElementAt(i).TestData;
+                    S_yy += Math.Pow(expData.ElementAt(i).TestData, 2);
+                    S_xy += expData.ElementAt(i).Elapse * expData.ElementAt(i).TestData / 1000;
+                }
+
+                double density = double.Parse(tbDensity.Text);
+
+                result = (expData.ElementAt(count - 1).TestData - expData.ElementAt(count - 2).TestData) / ((expData.ElementAt(count - 1).Elapse - expData.ElementAt(count - 2).Elapse) / 1000);
+
+                //result = (ytestdata[ytestdata.Count() - 1] - ytestdata[ytestdata.Count() - 2]) / (xtestdata[xtestdata.Count() - 1] - xtestdata[xtestdata.Count() - 2]);
+
+                //result = 15850.323141489 * System.Math.Abs(result / density);
+
+                Delta_m = expData.ElementAt(endpos - 1).TestData - expData.ElementAt(startpos).TestData;
+
+                Delta_t = (expData.ElementAt(endpos - 1).Elapse - expData.ElementAt(startpos).Elapse) / 1000;
+
+                lblFlowRate.Content = result.ToString("G4");
+
+                //double[] xdata = new double[count - 1];
+                //double[] ydata = new double[count - 1];
+
+                //for (int i = 1; i < count; i++)
+                //{
+                //    xdata[i - 1] = expData.ElementAt(i).Elapse / 1000;
+                //    ydata[i - 1] = expData.ElementAt(i).TestData;
+                //}
+
+
+                //MathNet.Numerics.Tuple<double, double> p = SimpleRegression.Fit(xdata, ydata);
+                //double a = p.Item1; // == 10; intercept
+                //double b = p.Item2; // == 0.5; slope
+
+                beta = (n * S_xy - S_x * S_y) / (n * S_xx - Math.Pow(S_x, 2));
+                alpha = S_y / n - beta * S_x / n;
+
+                Se_sigma2 = (1 / (n * (n - 2))) * (n * S_yy - Math.Pow(S_y, 2) - Math.Pow(beta, 2) * (n * S_xx - Math.Pow(S_x, 2)));
+
+                Se_beta2 = n * Se_sigma2 / (n * S_xx - Math.Pow(S_x, 2));
+
+                Se_sigma = Math.Sqrt(Se_beta2);
+
+                lblAvgFlowRate.Content = beta.ToString("G4");
+
+                lblStdev.Content = Se_sigma.ToString("G4");
+
+                Umassrep = (2 * Math.Sqrt(2) * (0.09 / 1000000) * 100) / Math.Abs(Delta_m);
+
+                Udeltam = (Math.Sqrt(Math.Pow(U_caluncer, 2) + Math.Pow(U_linearity, 2) + Math.Pow(U_sensitivity, 2) + Math.Pow(U_eccentricity, 2))) / 1000;
+
+                OneOverDeltaT = 1 / Delta_t;
+
+                Usigma = Se_sigma;
+
+                DTDM = -1 * Delta_m / (Math.Pow(Delta_t, 2));
+
+                Um = 2 * Math.Sqrt(Math.Pow((Udeltam * OneOverDeltaT),2) + Math.Pow((UdeltaT * DTDM) , 2) + Math.Pow(Usigma ,2));
+
+                Umrdg = 100 * (Um / Math.Abs(beta));
+                //   15850.323141489 * System.Math.Abs(b / density);
+
+                U_final = Math.Sqrt(Math.Pow(Umrdg, 2) + Math.Pow(Uimpurity, 2) + Math.Pow(Uhumidity, 2) + Math.Pow(Umassrep, 2) + Math.Pow(Utimerep, 2) + Math.Pow(Usysrep, 2));
+
+                lbluncer.Content = U_final;
+            }
         }
 
 
@@ -418,6 +512,82 @@ namespace DataLogger
 
             lblFlowRate.Content = n;
         }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog dlg = new System.Windows.Forms.OpenFileDialog();
+            dlg.DefaultExt = ".DAT";
+            dlg.Filter = "DAT Files|*.DAT";
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                 string filename = dlg.FileName;
+
+                 expData.Clear();
+                 
+                    string[] readText = File.ReadAllLines(filename);
+                    int strCount = 0;
+                    double currentElapse = 0;
+                    double prevElapse = 0;
+                    DateTime StartT = DateTime.Now;
+                    DateTime dT1;
+                    double mass;
+
+
+                    foreach(string s in readText)
+                    {
+                        string[] split1 = s.Split(new char[] { ',' });
+
+
+                        bool readStatus = true;
+                        if (split1.Count() > 1)
+                        {
+                            string[] split2 = split1[1].Split(new char[] { ' ' });
+
+                            if (split2.Count() > 4)
+                            {
+
+                                if (DateTime.TryParse(split1[0], out dT1) && double.TryParse(split2[3], out mass))
+                                {
+                                    if (strCount == 0)
+                                    {
+                                        StartT = dT1;
+                                    }
+
+                                    TimeSpan ds = dT1 - StartT;
+                                    prevElapse = currentElapse;
+                                    currentElapse = ds.TotalMilliseconds;
+
+                                    expData.Add(new TimeData(dT1, mass, ds.TotalMilliseconds, (currentElapse - prevElapse)));
+
+                                    Point p1 = new Point(ds.TotalMilliseconds, mass);
+                                    source1.AppendAsync(Dispatcher, p1);
+                                }
+                                else
+                                {
+                                    readStatus = false;
+                                }
+                            }
+                            else { readStatus = false; }
+                        }
+                        else { readStatus = false; }
+
+                        if (readStatus)
+                        {
+                            strCount++;
+                        }
+                    }
+
+                     GetInstFlowRate();
+
+                     lblDataCount.Content = expData.Count;
+            }
+
+        }
+
+
+        
 
     }
 }
